6c60e31b15a2d5c6e2739ef7f40afd14108291e4
SophiaMontenegro <sophiaazulmontenegro@gmail.com>
2022-10-19 20:42:12 -0600

    Change Name

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,");
         // Add a line here to print your name!
-        System.out.println("Marlon");
+	System.out.println("Sophia");
+        //System.out.println("Marlon");
     }
 }
